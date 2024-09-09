47b3d52a8a5059cd4a08fe9c8072e1e09d631eb1
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-09-10 01:36:06 +0300

    Initial commit

---
+++ src/main/java/com/aksoyakin/weatherappbe/WeatherAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.weatherappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aksoyakin/weatherappbe/WeatherAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.weatherappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
