5979ef2604634df2d4c13d3770fac5ae75dd72e1
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-09-11 22:26:36 +0300

    open api/swagger

--- src/main/java/com/aksoyakin/weatherappbe/controller/WeatherApi.java
+++ src/main/java/com/aksoyakin/weatherappbe/controller/WeatherApi.java
@@ -4,7 +4,14 @@ import com.aksoyakin.weatherappbe.controller.validation.CityNameConstraint;
 import com.aksoyakin.weatherappbe.dto.WeatherDto;
 import com.aksoyakin.weatherappbe.service.WeatherService;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/api/weather")
 @Validated
+@Tag(name = "Weather API v1", description = "Weather APP for filter by city for current temperature.")
 public class WeatherApi {
 
     private final WeatherService weatherService;
@@ -23,6 +31,28 @@ public class WeatherApi {
         this.weatherService = weatherService;
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Weather API summary.",
+            description = "Weather API for filter by city for current temperature.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = WeatherDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "BAD_REQUEST",
+                            content = @Content(
+                                    schema = @Schema(hidden = true)
+                            )
+                    )
+            }
+    )
     @GetMapping("/{city}")
     @RateLimiter(name = "basic")
     public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") @CityNameConstraint @NotBlank String city) {
